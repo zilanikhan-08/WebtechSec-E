bcc2832fbbcae4ffde943f478ac34dd258559d03
zilanikhan-08 <134102450+zilanikhan-08@users.noreply.github.com>
2024-02-20 17:27:16 +0600

    Add files via upload

---
+++ Lab3.1/1.php
@@ -0,0 +1,22 @@
+<?php
+$name = $_REQUEST["name"];
+
+if($name == "")
+{
+    echo "please provide your name";
+}
+else if(strlen($name)<2)
+{
+    echo "Wrong Input 1";
+} 
+else if($name[0]=='0'||$name[0]=='1'||$name[0]=='2'||$name[0]=='3'||$name[0]=='4'||$name[0]=='5'||$name[0]=='6'||$name[0]=='7'||$name[0]=='8'||$name[0]=='9' )
+{
+    echo "Wrong Input 2";
+} 
+else
+{
+    echo $name;
+}
+
+
+?>
